b08840dae2bf0071d8f6e5d404a2261433edcd37
jkdeepc <20109320@mail.wit.ie>
2024-10-11 20:57:52 +0800

    Rendered list of tasks using map

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -11,15 +11,19 @@ function App() {
     ]
   });
 
-  return (
-    <div className="container">
-      <h1>Tasky</h1>
-      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
-      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
-      
-    </div>
-  );
+ return (
+  <div className="container">
+    <h1>Tasky</h1>
+    {taskState.tasks.map((task, index) => (              
+      <Task 
+        key={index}
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+      />
+    ))} 
+  </div>
+);
 }
 
 export default App;
